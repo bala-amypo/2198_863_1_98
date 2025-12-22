c4f68f11b912b0ece76d1a3edb93f57c7a9e1afa
bala-amypo <bala@amypo.in>
2025-12-22 23:08:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Micro-Learning API is running! Use /courses, /lessons, /progress, /recommendations endpoints.";
+    }
+}
