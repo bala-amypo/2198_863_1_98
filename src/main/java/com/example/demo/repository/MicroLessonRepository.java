ee820dde79b82ac7027e0f507ffd7a10fe52114e
bala-amypo <bala@amypo.in>
2025-12-22 08:40:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/MicroLessonRepository.java
+++ src/main/java/com/example/demo/repository/MicroLessonRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MicroLessonRepository extends JpaRepository<MicroLesson, Long> {
-    List<MicroLesson> findByFilters(String tags, String difficulty, String contentType);
+
+    List<MicroLesson> findByTagsAndDifficultyAndContentType(
+            String tags,
+            String difficulty,
+            String contentType
+    );
 }
+
