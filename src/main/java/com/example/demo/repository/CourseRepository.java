cba4aa9e43743a6bdc5a81dc23268964f95dc3e6
bala-amypo <bala@amypo.in>
2025-12-22 12:24:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    boolean existsByTitleAndInstructorId(String title, Long instructorId);
-}
