14adab83250b0cc10d474b3f8dd52e2fbefee1c4
bala-amypo <bala@amypo.in>
2025-12-23 01:03:03 +0000

    User commit

--- src/main/java/com/example/demo/service/RecommendationService.java
+++ src/main/java/com/example/demo/service/RecommendationService.java
@@ -1,12 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Recommendation;
-import com.example.demo.model.User;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface RecommendationService {
-    Recommendation generateRecommendation(Long userId, String tags, String difficulty);
+    Recommendation generateRecommendation(Long userId);
     Recommendation getLatestRecommendation(Long userId);
     List<Recommendation> getRecommendations(Long userId, LocalDate from, LocalDate to);
 }
