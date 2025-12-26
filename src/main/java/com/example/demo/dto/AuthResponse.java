b249001c36311ddd3086f222958c5b9506fd75e2
bala-amypo <bala@amypo.in>
2025-12-26 16:56:00 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,10 +1,12 @@
 package com.example.demo.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class AuthResponse {
     private String accessToken;
 }
--- src/main/java/com/example/demo/dto/RecommendationRequest.java
+++ src/main/java/com/example/demo/dto/RecommendationRequest.java
@@ -1,10 +1,20 @@
 package com.example.demo.dto;
 
 import lombok.Data;
-
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 public class RecommendationRequest {
+
     private BigDecimal confidenceScore;
+
+    private List<String> tags;
+
+    private String targetDifficulty;
+
+    private String contentType;
+
+    private Integer maxItems;
 }
+    
